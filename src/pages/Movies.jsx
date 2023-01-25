b52a838f128b33886bcd5d271e2546cb374bd4e3
Maksym Boichenko <max.boychenkjo@gmail.com>
2023-01-25 19:14:08 +0200

    movie

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,11 +1,24 @@
 import { useState, useEffect } from 'react';
 
+import { Link, useSearchParams } from 'react-router-dom';
+
 import {} from '../../src/API/Api';
 
 export const Movies = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    // console.log(e);
+  };
+
+  const inputChange = e => {
+    setSearchParams(e.target.value ? { query: e.target.value } : {});
+  };
+
   return (
-    <form>
-      <input type="text" />
+    <form onSubmit={onSubmit}>
+      <input onChange={inputChange} type="text" />
     </form>
   );
 };
